1f65c7678207d696237a3b4ef87a4cdabb275ed5
kimdehwan <kimdehwan@175.123.196.230>
2017-06-01 00:07:06 +0900

    mybatis 연결 완료, 등록날짜 파싱 오류 발생 등록일 형식이 날짜가 바뀌면 hh:mm 에서 yy-mm-dd 로 바뀜. 해결중.

--- src/main/java/Crawling.java
+++ src/main/java/Crawling.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
@@ -10,21 +8,13 @@ public class Crawling {
 	public static void main(String args[]){
 		
 		try {
+			
 			List<Map<String,Object>> stocks = new CrawlingForFootSell().parsing();
 			
-			DataHandler dh = new DataHandler();
-			SqlSession session = dh.getSession();
-//			System.out.println(session.getConnection().getSchema());
+			SqlSession session = new DataHandler().getSession();
 			
-			session.insert("StocksMapper.insertTest",stocks);
-
-//			stocks.stream().forEach(shoes -> {
-//					System.out.println(shoes.get("id"));
-//					session.insert("StocksMapper.insertTest",  shoes);
-//			});
-			
-//			session.close();
-			
+			session.insert("StocksMapper.setMarketData",stocks);
+			session.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
